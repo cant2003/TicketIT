95687b72c33d4541d538e26943a24a4c51831c82
Cristian Nuñez <cn90336@gmail.com>
2026-04-21 15:03:06 -0400

    Update iniciador.bat

--- iniciador.bat
+++ iniciador.bat
@@ -1,16 +1,13 @@
 @echo off
-
 cd /d "%~dp0"
 
 call .\venv\Scripts\activate
 
-@echo off
-call venv\Scripts\activate
-
-start cmd /k uvicorn webhook_app:app --host 0.0.0.0 --port 8000
-start cmd /k python run_worker.py
-start cmd /k ngrok http 8000
+start "Webhook" cmd /k uvicorn webhook_app:app --host 0.0.0.0 --port 8000
+start "Worker" cmd /k python run_worker.py
+start "Ngrok" cmd /k ngrok http 8000
 
 echo.
-echo "EL BOT SE ESTA EJECUTANDO CORRECTAMENTE ruta ", "%~dp0"
+echo EL BOT SE ESTA EJECUTANDO CORRECTAMENTE
+echo Ruta: %~dp0
 pause
